dc67b5e389ea59e8ffa7378b33526e660bccab87
Zaiba Machhaliya <zaibamachhaliya@gmail.com>
2026-06-25 17:55:22 +0530

    feat: add rate limiting to auth routes

--- back-end/src/routes/authRoutes.js
+++ back-end/src/routes/authRoutes.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-router.post('/register', authController.register);
-router.post('/login', authController.login);
+const { loginLimiter, signupLimiter } = require("../middleware/rateLimit");
+
+
+router.post('/register', signupLimiter, authController.register);
+
+router.post('/login', loginLimiter, authController.login);
 
 module.exports = router;
